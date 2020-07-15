e70f72f03df1aeb03e60c5b3de08dcc659ab29c2
junha-ahn <junha.ahn.dev@gmail.com>
2020-07-15 22:25:53 +0900

    add: hello world

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+	console.log(`Server listening on port: ${PORT} (http://127.0.0.1:${PORT})`)
+})

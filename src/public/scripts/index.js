bad2a9f4d5a425e4674a4ac97376c28e2ef60167
junha-ahn <junha.ahn.dev@gmail.com>
2020-07-26 17:21:34 +0900

    add: PlayerList

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+/**
+ * @param  {Player[]} players 
+ * 
+ * example of players
+	[{
+
+		color: (3) [16.091078499940764, 239.86757401473753, 192.7068154206077]
+		endedAt: 1595751494926
+		id: "qanecFSgkuGS3VxsAAAB"
+		isMine: true
+		score: 12107
+		startedAt: 1595751482819
+	}]
+ */
+function PlayerList(players) {}
